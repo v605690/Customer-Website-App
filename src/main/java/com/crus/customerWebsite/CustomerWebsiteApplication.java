9f825712b6e907bf58bd6ed1813791ad4ca8d586
v605690 <v605690@gmail.com>
2025-07-25 13:12:37 -0500

    New Java App project - Customer Website

---
+++ src/main/java/com/crus/customerWebsite/CustomerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.crus.customerWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebsiteApplication.class, args);
+	}
+
+}
