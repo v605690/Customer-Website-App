c5effda212cddd970b839a76fcd75af7aa5c0c04
v605690 <v605690@gmail.com>
2025-07-26 12:32:52 -0500

    update customerWebsite/models/Customer, customerWebsite/repos/CustomerRepository, customerWebsite/services/CustomerService, customerWebsite/services/CustomerServiceImpl, application.properties

---
+++ src/main/java/com/crus/customerWebsite/models/Customer.java
@@ -0,0 +1,22 @@
+package com.crus.customerWebsite.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customers")
+@Builder
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String emailAddress;
+    private Integer age;
+    private String address;
+}
