5463d11c72e1324067ef05d8f5510a2e7405f708
v605690 <v605690@gmail.com>
2025-07-27 18:37:40 -0500

    update CustomerController, error.html

--- src/main/java/com/crus/customerWebsite/controllers/CustomerController.java
+++ src/main/java/com/crus/customerWebsite/controllers/CustomerController.java
@@ -41,7 +41,19 @@ public class CustomerController {
     // creates a Customer based on the object you collected from
     // the HTML page above
 
-    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+    public String saveCustomer(@ModelAttribute("customer") Customer customer, Model model) {
+
+        if (customer == null) {
+            model.addAttribute("message", "Customer cannot be null");
+
+            return "error";
+        }
+        if (customer.getFullName() == null || customer.getFullName().isEmpty()) {
+            model.addAttribute("message", "Customer name cannot be empty");
+
+            return "error";
+        }
+
         customerService.saveCustomer(customer);
         return "redirect:/";
     }
@@ -50,13 +62,26 @@ public class CustomerController {
     // the path variable "id" is used to pull a customer from the databas
     public ModelAndView showEditCustomerPage(@PathVariable(name = "id") Long id) {
 
+        Customer customer = customerService.getCustomer(id);
+
+        if (customer == null) {
+            ModelAndView mav = new ModelAndView("error");
+            mav.addObject("Customer with ID " + id + " not found");
+            return mav;
+        }
+        if (!id.equals(customer.getId())) {
+            ModelAndView mav2 = new ModelAndView("error");
+            mav2.addObject("ID mismatch: URL ID " + id +
+                    " does not match customer ID " + customer.getId());
+            return mav2;
+        }
+
         // since the previous methods use Model, this one uses ModelAndView
         // to get some experience using both. Model is more common these days,
         // but ModelAndView accomplishes the same thing and can be useful in
         // certain circumstances. The view name is passed to the constructor.
 
         ModelAndView mav = new ModelAndView("edit-customer");
-        Customer customer = customerService.getCustomer(id);
         mav.addObject("customer", customer);
         return mav;
     }
@@ -69,7 +94,7 @@ public class CustomerController {
             model.addAttribute("message",
                     "Cannot update, customer id " + customer.getId()
             + " doesn't match id to update: " + id + ".");
-            return "error-page";
+            return "error";
         }
 
         customerService.saveCustomer((customer));
@@ -78,6 +103,7 @@ public class CustomerController {
 
     @RequestMapping("/delete/{id}")
     public String deleteCustomer(@PathVariable(name = "id") Long id) {
+
         customerService.deleteCustomer(id);
         return "redirect:/";
     }
